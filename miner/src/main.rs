5f6aa016a557ae1141adc2e608f69674982f7449
Mihir Kumar Singh <mihir39nr@gmail.com>
2024-04-29 22:00:50 +0530

    attempt-17

--- miner/src/main.rs
+++ miner/src/main.rs
@@ -1,41 +1,52 @@
 use miner::structs::Transaction;
 use miner::utils::print_hex_string;
-use miner::{calculate_merkle_root, create_block_header, create_coinbase_trx, mine_block, print_soln, get_compact_size};
+use miner::{
+    calculate_merkle_root, create_block_header, create_coinbase_trx, get_compact_size, mine_block,
+    print_soln,
+};
 use std::error::Error;
 use std::fs::read_dir;
 fn main() -> Result<(), Box<dyn Error>> {
     let directory_path = "../mempool";
     let mut txids: Vec<Vec<u8>> = Vec::new();
     let mut transactions: Vec<u8> = Vec::new();
-    let (trx,crx) = create_coinbase_trx();
+    let (trx, crx) = create_coinbase_trx();
     transactions.extend(&crx);
     txids.push(crx);
+    let mut trans = 0;
     for entry in read_dir(directory_path)? {
         let entry = entry?;
         let path = entry.path();
+        
         if path.is_file() && path.extension().unwrap() == "json" {
-            let transaction = Transaction::parse_from_file(path.to_str().unwrap())?;
+            if trans < 1500 {
+                let transaction = Transaction::parse_from_file(path.to_str().unwrap())?;
 
-            // println!("--- Parsed file: {}", path.display());
-            let isegwit = transaction.valid_trans();
-            if isegwit.0 {
-                let serialized_data = transaction.get_data(isegwit.1);
-                // print!("Hashed Data ");
-                // print_hex_string(&serialized_data);
-                transactions.extend(serialized_data);
-                // print!("Transaction Id ");
-                let txis = transaction.get_txid();
-                
-                transactions.extend(&txis);
-                // print_hex_string(&txis);
-                txids.push(txis);
+                // println!("--- Parsed file: {}", path.display());
+                let isegwit = transaction.valid_trans();
+                if isegwit.0 {
+                    let serialized_data = transaction.get_data(isegwit.1);
+                    // print!("Hashed Data ");
+                    // print_hex_string(&serialized_data);
+                    transactions.extend(serialized_data);
+                    // print!("Transaction Id ");
+                    let txis = transaction.get_txid();
+
+                    transactions.extend(&txis);
+                    // print_hex_string(&txis);
+                    txids.push(txis);
+                }
+                trans += 1;
+            }
+            else {
+                continue;
             }
         }
     }
     let merkle_root = calculate_merkle_root(&txids);
     let block_header = create_block_header(merkle_root);
-    let (block,_nonce) = mine_block(&block_header);
+    let (block, _nonce) = mine_block(&block_header);
     // print_hex_string(&block);
-    print_soln(&block,&trx, &txids);
+    print_soln(&block, &trx, &txids);
     Ok(())
 }
